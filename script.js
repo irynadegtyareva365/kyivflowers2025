a43e77eee1b0f033b6044fbc5beaec4b62c6f46c
irynadegtyareva365 <bitokrodion@gmail.com>
2025-09-30 00:04:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const flowers = [
-    { id: 1, name: 'Троянди', description: 'Класичний букет червоних троянд, символ кохання та пристрасті.', price: 500, image: 'https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 2, name: 'Тюльпани', description: 'Яскравий весняний букет тюльпанів різних кольорів.', price: 300, image: 'https://images.pexels.com/photos/931177/pexels-photo-931177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 3, name: 'Півонії', description: 'Пишний букет рожевих півоній, що додає романтики.', price: 600, image: 'https://images.pexels.com/photos/160852/pexels-photo-160852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 4, name: 'Орхідеї', description: 'Екзотичний букет орхідей для особливих моментів.', price: 700, image: 'https://images.pexels.com/photos/1335971/pexels-photo-1335971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 5, name: 'Гортензії', description: 'Ніжний букет гортензій у пастельних тонах.', price: 450, image: 'https://images.pexels.com/photos/944697/pexels-photo-944697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 6, name: 'Гербери', description: 'Веселий букет гербер, що піднімає настрій.', price: 350, image: 'https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 7, name: 'Лілії', description: 'Елегантний букет лілій з приємним ароматом.', price: 550, image: 'https://images.pexels.com/photos/68507/spring-flowers-flowers-collage-68507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 8, name: 'Хризантеми', description: 'Осінній букет хризантем для затишку.', price: 400, image: 'https://images.pexels.com/photos/67857/daisy-flower-spring-meadow-67857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 1, name: 'Троянди', description: 'Класичний букет червоних троянд, символ кохання та пристрасті.', price: 500, image: 'https://images.pexels.com/photos/19269253/pexels-photo-19269253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 2, name: 'Тюльпани', description: 'Яскравий весняний букет тюльпанів різних кольорів.', price: 300, image: 'https://images.pexels.com/photos/7664379/pexels-photo-7664379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 3, name: 'Півонії', description: 'Пишний букет рожевих півоній, що додає романтики.', price: 600, image: 'https://images.pexels.com/photos/10266847/pexels-photo-10266847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 4, name: 'Орхідеї', description: 'Екзотичний букет орхідей для особливих моментів.', price: 700, image: 'https://images.pexels.com/photos/11577348/pexels-photo-11577348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 5, name: 'Гортензії', description: 'Ніжний букет гортензій у пастельних тонах.', price: 450, image: 'https://images.pexels.com/photos/18703647/pexels-photo-18703647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 6, name: 'Гербери', description: 'Веселий букет гербер, що піднімає настрій.', price: 350, image: 'https://images.pexels.com/photos/17555771/pexels-photo-17555771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 7, name: 'Лілії', description: 'Елегантний букет лілій з приємним ароматом.', price: 550, image: 'https://images.pexels.com/photos/16687847/pexels-photo-16687847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { id: 8, name: 'Хризантеми', description: 'Осінній букет хризантем для затишку.', price: 400, image: 'https://images.pexels.com/photos/14145345/pexels-photo-14145345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
     { id: 9, name: 'Альстромерії', description: 'Довговічний букет альстромерій у яскравих кольорах.', price: 320, image: 'https://images.pexels.com/photos/36729/alstroemeria-peruvian-lily-flowers-plant.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: 10, name: 'Гвоздики', description: 'Класичний букет гвоздик для свят.', price: 280, image: 'https://images.pexels.com/photos/1418378/pexels-photo-1418378.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
+    { id: 10, name: 'Гвоздики', description: 'Класичний букет гвоздик для свят.', price: 280, image: 'https://images.pexels.com/photos/894753/pexels-photo-894753.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
 ];
 
 let cart = [];
